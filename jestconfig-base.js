f08a2b1bef5d9b612a7dccdf8b7ce0ae9973acf5
giannif <giannif@gmail.com>
2019-04-12 10:08:15 -0700

    build(clean-up)

--- jestconfig-base.js
+++ jestconfig-base.js
@@ -1,7 +1,7 @@
 module.exports = {
     roots: ['<rootDir>/src'],
     transform: {
-        '^.+\\.(t|j)sx?$': 'ts-jest',
+        '^.+\\.tsx?$': 'ts-jest',
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
