de6047208b1ba20fc1e1b3924d0ef744c5f04a15
giannif <giannif@gmail.com>
2019-09-16 09:54:13 -0400

    fix(fetch-api): eslint and async in promise

--- packages/fetch-api/src/request.ts
+++ packages/fetch-api/src/request.ts
@@ -5,40 +5,50 @@ export const DEFAULT_ERROR = 'Error fetching'
 const serverUrl = 'https://api.giphy.com/v1/'
 const identity = (i: any) => i
 const requestMap: { [key: string]: Promise<Result> } = {}
+
+class FetchError extends Error {
+    statusText: string
+    status: number
+    constructor(message: string, status: number = 0, statusText: string = '') {
+        super(message) // (1)
+        this.status = status
+        this.statusText = statusText
+    }
+}
 function request(
     url: string,
     normalizer: (a: any, pingbackType?: PingbackEventType) => any = identity,
     pingbackType?: PingbackEventType,
 ) {
     if (!requestMap[url]) {
-        requestMap[url] = new Promise<Result>(async (resolve, reject) => {
+        const makeRequest = async (): Promise<Result> => {
+            let fetchError: FetchError
             try {
                 const response = await fetch(`${serverUrl}${url}`, { method: 'get' })
                 if (response.ok) {
                     const result = (await response.json()) as Result
-                    resolve(normalizer(result, pingbackType))
+                    return normalizer(result, pingbackType)
                 } else {
+                    // handle errors
                     let message = DEFAULT_ERROR
                     try {
                         // error results have a different format than regular results
                         const result = (await response.json()) as ErrorResult
                         if (result.message) message = result.message
                     } catch (_) {}
-                    reject({
-                        status: response.status,
-                        statusText: response.statusText,
-                        message: `${ERROR_PREFIX}${message}`,
-                    })
+                    fetchError = new FetchError(`${ERROR_PREFIX}${message}`, response.status, response.statusText)
                 }
-            } catch (error) {
-                reject({ message: error.message })
+            } catch (unexpectedError) {
+                fetchError = new FetchError(unexpectedError.message)
                 // if the request fails with an unspecfied error,
                 // the user can request again
                 // TODO: perhaps we can return a function to clear
                 // { clearCache: () => delete requestMap[url] }
                 delete requestMap[url]
             }
-        })
+            throw fetchError
+        }
+        requestMap[url] = makeRequest()
     }
     return requestMap[url]
 }
