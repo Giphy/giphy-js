1a563a209858306ce9b3a8abfa1e052fd7468848
giannif <giannif@gmail.com>
2020-02-26 09:32:47 -0800

    upgrade(preact): no more next use semver

--- packages/components/src/util/observer.tsx
+++ packages/components/src/util/observer.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 class Observer extends Component<Props, State> {
     io: any
-    container?: HTMLElement
+    container: HTMLElement | null = null
     componentDidMount() {
         this.io = new IntersectionObserver(([entry]: IntersectionObserverEntry[]) => {
             this.setState({ isVisible: entry.isIntersecting })
