b072a99f4ff1da41b3c19d8dc678f2ee96f7f6f6
giannif <giannif@gmail.com>
2020-03-31 12:24:09 -0700

    fix(node-support): don't polyfill headers
    let headers be undefined
    if headers need to be polyfilled, fetch needs a polyfill too
    let the consumer of the package choose their polyfill

--- packages/util/src/sdk-headers.ts
+++ packages/util/src/sdk-headers.ts
@@ -1,9 +1,4 @@
-// @ts-ignore
-// eslint-disable-next-line
-import 'unfetch'
-
 type GiphySDKRequestHeader = 'X-GIPHY-SDK-NAME' | 'X-GIPHY-SDK-VERSION' | 'X-GIPHY-SDK-PLATFORM'
-// hi!
 const gl = (window || global || {}) as any
 // define _GIPHY_SDK_HEADERS_ if they don't exist
 gl._GIPHY_SDK_HEADERS_ =
@@ -14,7 +9,7 @@ gl._GIPHY_SDK_HEADERS_ =
           })
         : undefined)
 
-export const getGiphySDKRequestHeaders = () => gl._GIPHY_SDK_HEADERS_
+export const getGiphySDKRequestHeaders = (): Headers | undefined => gl._GIPHY_SDK_HEADERS_
 
 export const appendGiphySDKRequestHeader = (key: GiphySDKRequestHeader, value: string) =>
     getGiphySDKRequestHeaders()?.set(key, value)
