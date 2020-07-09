9533b7a6fd82e179ceac4a6280ab711bdf91654b
giannif <giannif@gmail.com>
2020-07-09 12:37:57 -0700

    fix(react-components): column offsets in masonry

--- packages/react-components/src/components/masonry-grid.tsx
+++ packages/react-components/src/components/masonry-grid.tsx
@@ -1,7 +1,7 @@
 import React, { memo, ReactNode } from 'react'
 
-function fillArray(length: any) {
-    return Array.apply(null, Array(length)).map(() => 0)
+function fillArray(length: number, columnOffsets: number[] = []) {
+    return Array.apply(null, Array(length)).map((_, index) => columnOffsets[index] || 0)
 }
 
 type Props = {
@@ -25,14 +25,13 @@ const MasonryGrid = ({
     const containerStyle: any = {}
     function getChildren() {
         let columnTarget: number
-        const columnHeights: number[] = fillArray(columns)
+        const columnHeights: number[] = fillArray(columns, columnOffsets)
         const result = React.Children.map(children, (child: React.ReactNode, index: number) => {
             const style: any = {
                 position: 'absolute',
             }
             columnTarget = columnHeights.indexOf(Math.min.apply(Math, columnHeights))
-            const topOffset = columnOffsets[columnTarget] || 0
-            const top = `${columnHeights[columnTarget] + topOffset}px`
+            const top = `${columnHeights[columnTarget]}px`
             const left = `${columnTarget * itemWidth + columnTarget * gutter}px`
             if (useTransform) {
                 style.transform = `translate3d(${left}, ${top}, 0)`
