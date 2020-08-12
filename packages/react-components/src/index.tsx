2e922b8eac11702066ab7f4230493ff5a87ba749
giannif <giannif@gmail.com>
2020-08-12 10:07:49 -0700

    cleanup(react-components): remove legacy grid

--- packages/react-components/src/index.tsx
+++ packages/react-components/src/index.tsx
@@ -6,7 +6,6 @@ export { default as AttributionOverlay } from './components/attribution/overlay'
 export { default as Carousel } from './components/carousel'
 export { default as Gif, GifOverlayProps, PingbackContext } from './components/gif'
 export { default as Grid } from './components/grid'
-export { default as LegacyGrid } from './components/legacy-grid'
 
 const { version } = require('../package.json')
 
