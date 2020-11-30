5d1131098b924a803cb4ce954fe06371a431c237
giannif <giannif@gmail.com>
2020-11-30 07:58:19 -0800

    fix(search-bar): condensed mode

--- packages/react-components/src/components/search-bar/theme.ts
+++ packages/react-components/src/components/search-bar/theme.ts
@@ -1,14 +1,14 @@
 import { css } from '@emotion/core'
 import styled, { CreateStyled } from '@emotion/styled'
 
-export const mobileQuery = `max-width: 480px`
-
 export type SearchTheme = {
     mode: 'dark' | 'light'
     // the height of the search bar on desktop
     searchbarHeight: number
     // the height of the search bar on mobile
     smallSearchbarHeight: number
+    condensedMediaQuery?: string
+    condensedMode?: boolean
 }
 
 export const initTheme = (theme?: Partial<SearchTheme>): SearchTheme => {
@@ -16,6 +16,8 @@ export const initTheme = (theme?: Partial<SearchTheme>): SearchTheme => {
         mode: 'light',
         searchbarHeight: 42,
         smallSearchbarHeight: 35,
+        // bump the condensedMediaQuery to force if condensedMode is true
+        condensedMediaQuery: theme?.condensedMode ? `max-width: 99999px` : `max-width: 480px`,
         ...theme,
     }
 }
@@ -27,7 +29,7 @@ export const getSize = (theme: SearchTheme, includeWidth: boolean = false) => cs
     css`
         width: ${theme.searchbarHeight}px;
     `};
-    @media (${mobileQuery}) {
+    @media (${theme.condensedMediaQuery}) {
         height: ${theme.smallSearchbarHeight}px;
         ${includeWidth &&
         css`
