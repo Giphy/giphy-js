b4050d72180613cd5f45f8940770d0e07eb2995e
Daniel Leite de Oliveira <dleitee@gmail.com>
2021-01-27 18:03:02 -0300

    nit: Replace ?? by ||

--- packages/analytics/src/send-pingback.ts
+++ packages/analytics/src/send-pingback.ts
@@ -3,7 +3,7 @@ import { PingbackEvent } from './types'
 import gl from './global'
 
 // TODO remove api key
-const environment = gl?.GIPHY_PINGBACK_URL ?? 'https://pingback.giphy.com'
+const environment = gl?.GIPHY_PINGBACK_URL || 'https://pingback.giphy.com'
 const pingBackUrl = `${environment}/v2/pingback?apikey=l0HlIwPWyBBUDAUgM`
 
 export const sendPingback = (events: PingbackEvent[]) => {
