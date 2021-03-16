2630cb827e1dcfd9238a2c101197b2c2bd2319ae
giannif <giannif@gmail.com>
2021-03-16 09:14:07 +0900

    fix(react-native): add checks for unsupported stuff

--- packages/util/src/log.ts
+++ packages/util/src/log.ts
@@ -7,25 +7,28 @@ export enum LogLevel {
 }
 /* istanbul ignore next */
 export const Logger = {
-    ENABLED: typeof window !== 'undefined' && location && location.search.indexOf('giphy-debug') !== -1,
+    ENABLED:
+        typeof window !== 'undefined' &&
+        typeof location !== 'undefined' &&
+        location.search.indexOf('giphy-debug') !== -1,
     LEVEL: 0,
     PREFIX: 'GiphyJS',
-    debug: function(...msg: any) {
+    debug: (...msg: any) => {
         if (Logger.ENABLED && Logger.LEVEL <= LogLevel.DEBUG) {
             console.debug(Logger.PREFIX, ...msg) // eslint-disable-line no-console
         }
     },
-    info: function(...msg: any) {
+    info: (...msg: any) => {
         if (Logger.ENABLED && Logger.LEVEL <= LogLevel.INFO) {
             console.info(Logger.PREFIX, ...msg) // eslint-disable-line no-console
         }
     },
-    warn: function(...msg: any) {
+    warn: (...msg: any) => {
         if (Logger.ENABLED && Logger.LEVEL <= LogLevel.WARN) {
             console.warn(Logger.PREFIX, ...msg) // eslint-disable-line no-console
         }
     },
-    error: function(...msg: any) {
+    error: (...msg: any) => {
         if (Logger.ENABLED && Logger.LEVEL <= LogLevel.ERROR) {
             console.error(Logger.PREFIX, ...msg) // eslint-disable-line no-console
         }
