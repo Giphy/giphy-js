5d6a9f21282953bf3bbcbb99f5ef7b6e9bda4291
giannif <giannif@gmail.com>
2021-03-26 14:29:15 +0900

    feat(react-components): video overlay supportand play with sound

--- packages/react-components/src/components/video/video-overlay.tsx
+++ packages/react-components/src/components/video/video-overlay.tsx
@@ -1,15 +1,64 @@
 import styled from '@emotion/styled'
-import React from 'react'
+import React, { useState } from 'react'
 import Video from '.'
 import { GifOverlayProps } from '../gif'
 
+const VideoContainer = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+`
 const VideoStyled = styled(Video)`
     height: 100%;
     display: inline-block;
     object-fit: cover;
 `
 
+const Button = styled.div`
+    background: black;
+    color: white;
+    padding: 10px;
+`
+
 const VideoOverlay = ({ gif, width, height, isHovered }: GifOverlayProps) => {
-    return isHovered ? <VideoStyled gif={gif} key={gif.id} loop muted={false} width={width} height={height} /> : null
+    const [muted, setMuted] = useState<boolean | undefined>(undefined)
+    const [mutedByBrowser, setMutedByBrowser] = useState(false)
+    const toggleMute = () => {
+        if (mutedByBrowser) {
+            setMutedByBrowser(false)
+            setMuted(false)
+        } else {
+            setMuted(!muted)
+        }
+    }
+    return (
+        <VideoContainer>
+            <Button
+                onClick={(e) => {
+                    e.preventDefault()
+                    toggleMute()
+                }}
+            >
+                {muted ? 'user muted' : 'user not muted'}
+                <br />
+                {mutedByBrowser ? 'muted by browser' : 'not muted by browser'}
+            </Button>
+            {isHovered ? (
+                <VideoStyled
+                    gif={gif}
+                    key={gif.id}
+                    loop
+                    muted={muted}
+                    width={width}
+                    height={height}
+                    onMuted={setMutedByBrowser}
+                />
+            ) : null}
+        </VideoContainer>
+    )
 }
 export default VideoOverlay
