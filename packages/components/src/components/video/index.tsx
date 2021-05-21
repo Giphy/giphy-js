f9f70fdab27454c0e1eb8ac8940ef71cd2375c1e
giannif <giannif@gmail.com>
2021-05-22 08:26:22 +0900

    fix(video): only fire pingbacks with arp

--- packages/components/src/components/video/index.tsx
+++ packages/components/src/components/video/index.tsx
@@ -124,7 +124,9 @@ const Video = ({
         onStateChange?.('playing')
         if (!hasPlayingFired.current) {
             hasPlayingFired.current = true
-            pingback({ actionType: 'START', analyticsResponsePayload: gif.analytics_response_payload })
+            if (gif.analytics_response_payload) {
+                pingback({ actionType: 'START', analyticsResponsePayload: gif.analytics_response_payload })
+            }
             onFirstPlay?.(Date.now() - mountTime.current)
         }
     }, [onFirstPlay, onStateChange])
