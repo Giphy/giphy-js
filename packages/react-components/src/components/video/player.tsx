9f7c339cd97dbcf5b80a8b14010ee27d9160ce5b
giannif <giannif@gmail.com>
2021-06-18 08:42:01 -0700

    video controls

---
+++ packages/react-components/src/components/video/player.tsx
@@ -0,0 +1,109 @@
+import styled from '@emotion/styled'
+import React, { ComponentProps, useCallback, useState } from 'react'
+import { PauseIcon, PlayIcon } from './controls/play-pause'
+import { VolumeOffIcon, VolumeOnIcon } from './controls/volume'
+import Video, { MEDIA_STATE } from './index'
+
+const Container = styled.div`
+    position: relative;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    .${Video.className} {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
+`
+
+const PlayPause = styled.div``
+
+const Volume = styled.div``
+
+const Controls = styled.div<{ isHovered: boolean }>`
+    position: absolute;
+    bottom: 5px;
+    right: 5px;
+    left: 5px;
+    display: flex;
+    justify-content: space-between;
+    opacity: ${(props) => (props.isHovered ? 1 : 0)};
+    transition: opacity ease-out 250ms;
+`
+
+const VideoPlayer = (props: ComponentProps<typeof Video>) => {
+    const [isHovered, setIsHovered] = useState(false)
+    const [playState, setPlayState] = useState<MEDIA_STATE>('paused')
+    const [videoEl, _setVideoEl] = useState<HTMLVideoElement | null>(null)
+    const [muted, setMuted] = useState<boolean | undefined>(undefined)
+    const [mutedByBrowser, setMutedByBrowser] = useState(false)
+    const { onStateChange, setVideoEl, onMuted } = props
+    const combinedSetPlayState = useCallback(
+        (args) => {
+            onStateChange?.(args)
+            setPlayState(args)
+        },
+        [onStateChange, setPlayState]
+    )
+    const combinedOnMuted = useCallback(
+        (args) => {
+            onMuted?.(args)
+            setMutedByBrowser(args)
+        },
+        [setMutedByBrowser, onMuted]
+    )
+    const combinedSetVideoEl = useCallback(
+        (args) => {
+            setVideoEl?.(args)
+            _setVideoEl(args)
+        },
+        [setVideoEl, _setVideoEl]
+    )
+    const toggleMute = () => {
+        if (mutedByBrowser) {
+            setMutedByBrowser(false)
+            setMuted(false)
+        } else {
+            setMuted(!muted)
+        }
+    }
+    return (
+        <Container
+            style={{ width: props.width }}
+            onMouseOver={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <Video
+                {...props}
+                onStateChange={combinedSetPlayState}
+                onMuted={combinedOnMuted}
+                setVideoEl={combinedSetVideoEl}
+                muted={muted}
+            />
+            <Controls isHovered={isHovered}>
+                {videoEl && (
+                    <PlayPause
+                        onClick={() => {
+                            if (playState === 'playing') videoEl.pause()
+                            else videoEl.play()
+                        }}
+                    >
+                        {playState === 'playing' ? <PauseIcon /> : <PlayIcon />}
+                    </PlayPause>
+                )}
+                <Volume
+                    onClick={(e) => {
+                        e.preventDefault()
+                        toggleMute()
+                    }}
+                >
+                    {muted || mutedByBrowser ? <VolumeOffIcon /> : <VolumeOnIcon />}
+                </Volume>
+            </Controls>
+        </Container>
+    )
+}
+export default VideoPlayer
---
+++ packages/react-components/stories/video-player.stories.tsx
@@ -0,0 +1,40 @@
+import { GiphyFetch } from '@giphy/js-fetch-api'
+import { IGif } from '@giphy/js-types'
+import isPercy from '@percy-io/in-percy'
+import { boolean, number, text, withKnobs } from '@storybook/addon-knobs'
+import React, { useCallback, useEffect, useState } from 'react'
+import { jsxDecorator } from 'storybook-addon-jsx'
+import VideoPlayer from '../src/components/video/player'
+
+const gf = new GiphyFetch('sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh')
+
+type Props = { id: string; width: number; height?: number; muted: boolean }
+const VideoDemo = ({ id, width }: Props) => {
+    const [gif, setGif] = useState<IGif>()
+
+    const fetch = useCallback(async () => {
+        const { data: gif } = await gf.gif(id)
+        setGif(gif)
+    }, [id])
+
+    useEffect(() => {
+        fetch()
+    }, [fetch, id])
+
+    // percy and our video autoplay aren't cooperating
+    return !isPercy() && gif ? <VideoPlayer gif={gif} width={width} /> : null
+}
+
+export default {
+    title: 'React Components/Video Player',
+    decorators: [withKnobs, jsxDecorator],
+}
+
+export const Video = () => (
+    <VideoDemo
+        id={text('id', 'D068R9Ziv1iCjezKzG')}
+        width={number('width', 300)}
+        height={number('height', 0)}
+        muted={boolean('muted', false)}
+    />
+)
