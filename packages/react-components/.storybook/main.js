7487ceccdd33f0ffbdaf5bf9fcd0b199be74d511
giannif <giannif@gmail.com>
2021-07-01 09:42:37 -0400

    react 17 peer dep / storybook upgrade

---
+++ packages/react-components/.storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+    stories: ['../**/*.stories.tsx'],
+    addons: [
+        'storybook-addon-jsx/register',
+        '@storybook/addon-viewport/register',
+        '@storybook/addon-actions/register',
+        '@storybook/addon-knobs',
+    ],
+}
