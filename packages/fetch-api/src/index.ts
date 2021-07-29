19ef5730fd81f29ed2003926d421009b10b24f9c
giannif <giannif@gmail.com>
2021-07-29 10:50:40 -0700

    feat(fetch-api): export serverUrl
    DRY and for testing environments that use GIPHY_API_URL

--- packages/fetch-api/src/index.ts
+++ packages/fetch-api/src/index.ts
@@ -1,9 +1,15 @@
-import { getGiphySDKRequestHeaders, appendGiphySDKRequestHeader } from '@giphy/js-util'
+import { appendGiphySDKRequestHeader, getGiphySDKRequestHeaders } from '@giphy/js-util'
+
+/* istanbul ignore next */
+const gl = ((typeof window !== 'undefined' ? window : global) || {}) as any
+// GIPHY_API_URL is made to override the api url in testing environments
+// must be specified before the packages load
+export const serverUrl = gl.GIPHY_API_URL || 'https://api.giphy.com/v1/'
 
 export { default as GiphyFetch } from './api'
 export * from './option-types'
-export * from './result-types'
 export { gifPaginator } from './paginator'
+export * from './result-types'
 
 const { version } = require('../package.json')
 // since we have multiple SDKs, we're defining a hieracrchy
--- packages/fetch-api/src/request.ts
+++ packages/fetch-api/src/request.ts
@@ -1,11 +1,9 @@
+import { serverUrl } from '.'
 import FetchError from './fetch-error'
 import { ErrorResult, Result } from './result-types'
+
 export const ERROR_PREFIX = `@giphy/js-fetch-api: `
 export const DEFAULT_ERROR = 'Error fetching'
-
-/* istanbul ignore next */
-const gl = ((typeof window !== 'undefined' ? window : global) || {}) as any
-const serverUrl = gl.GIPHY_API_URL || 'https://api.giphy.com/v1/'
 
 const identity = (i: any) => i
 const requestMap: {
