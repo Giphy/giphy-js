d45d20d96edc5db3628dad0624b433ea156e7e71
giannif <giannif@gmail.com>
2023-05-15 11:01:48 +0200

    add intersection observer polyfill

--- packages/react-components/src/index.tsx
+++ packages/react-components/src/index.tsx
@@ -1,24 +1,23 @@
 import { appendGiphySDKRequestHeader } from '@giphy/js-util'
-import 'intersection-observer'
-
+import './pollyfill'
 export { default as Attribution } from './components/attribution'
 export { default as AttributionOverlay } from './components/attribution/overlay'
 export { default as VerifiedBadge } from './components/attribution/verified-badge'
 export { default as Carousel } from './components/carousel'
+export { EmojiVariationsList } from './components/emoji-variations-list'
+export type { EmojiVariationsListProps } from './components/emoji-variations-list'
 export { default as Gif } from './components/gif'
 export { default as Grid } from './components/grid'
 export { default as Loader } from './components/loader'
 export { PingbackContext } from './components/pingback-context-manager'
 export { default as SearchBar } from './components/search-bar'
-export { default as SearchContextManager, SearchContext } from './components/search-bar/context'
+export { SearchContext, default as SearchContextManager } from './components/search-bar/context'
 export { default as SuggestionBar } from './components/search-bar/suggestion-bar'
 export type { GifOverlayProps } from './components/types'
 export { default as Video } from './components/video'
 export * from './components/video/controls/play-pause'
 export * from './components/video/controls/volume'
 export { default as VideoOverlay } from './components/video/video-overlay'
-export type { EmojiVariationsListProps } from './components/emoji-variations-list'
-export { EmojiVariationsList } from './components/emoji-variations-list'
 
 const { version } = require('../package.json')
 
