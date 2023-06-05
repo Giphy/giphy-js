579b06c5fd1dee0bbca89f7ada85cd2060e7f690
giannif <giannif@gmail.com>
2023-06-05 18:58:43 +0200

    svelte components grid

--- packages/svelte-components/src/lib/Grid.svelte
+++ packages/svelte-components/src/lib/Grid.svelte
@@ -1,28 +1,56 @@
 <script lang="ts">
+    import { gifPaginator, type GifsResult } from '@giphy/js-fetch-api'
     import type { IGif } from '@giphy/js-types'
     import { getGifHeight } from '@giphy/js-util'
+    import { onMount } from 'svelte'
+    import { debounce } from 'throttle-debounce'
     import Gif from './Gif.svelte'
     export let initialGifs: IGif[] = []
+    export let fetchGifs: (offset: number) => Promise<GifsResult>
     export let width: number = 600
-    export let columns: number = 3
+    export let columns: number = 4
     export let gutter: number = 6
     let columnTarget: number
     const gutterOffset = gutter * (columns - 1)
     const gifWidth = Math.floor((width - gutterOffset) / columns)
-    const gifHeights = initialGifs.map((gif) => getGifHeight(gif, gifWidth))
+    $: gifHeights = initialGifs.map((gif) => getGifHeight(gif, gifWidth))
+    $: isLoaderVisible = false
+    $: isFetching = false
     let columnHeights: number[] = Array.apply(null, Array(columns)).map((_) => 0)
-    let containerHeight = Math.max.apply(Math, columnHeights) - gutter
+    let containerHeight: number
     function getStyle(index: number) {
-        columnTarget = columnHeights.indexOf(Math.min.apply(Math, columnHeights))
+        columnTarget = columnHeights.indexOf(Math.min(...columnHeights))
         const top = columnHeights[columnTarget]
         const left = columnTarget * gifWidth + columnTarget * gutter
         const height = gifHeights[index]
         if (height) {
             columnHeights[columnTarget] += height + gutter
         }
-        containerHeight = Math.max.apply(Math, columnHeights) - gutter
+        containerHeight = Math.max(...columnHeights) - gutter
         return `translate3d(${left}px, ${top}px, 0)`
     }
+
+    const doFetch = debounce(100, async () => {
+        if (isLoaderVisible && !isFetching) {
+            isFetching = true
+            const gifs = await paginator()
+            initialGifs = gifs
+            isFetching = false
+        }
+    })
+
+    // when these change fetch more gifs
+    $: initialGifs, isLoaderVisible, isFetching, doFetch()
+
+    let loader: HTMLDivElement
+    let paginator = gifPaginator(fetchGifs, initialGifs)
+    onMount(() => {
+        const i = new IntersectionObserver(([entry]: IntersectionObserverEntry[]) => {
+            isLoaderVisible = entry.isIntersecting
+        })
+        i.observe(loader)
+        return () => i.disconnect()
+    })
 </script>
 
 <div class="container" style="width:{width}px; height:{containerHeight}px;">
@@ -32,8 +60,14 @@
         </div>
     {/each}
 </div>
+<div class="loader" bind:this={loader} />
 
 <style>
+    .loader {
+        width: 100px;
+        height: 50px;
+        background: red;
+    }
     .container {
         overflow: hidden;
         position: relative;
--- packages/svelte-components/src/routes/grid/+page.svelte
+++ packages/svelte-components/src/routes/grid/+page.svelte
@@ -2,13 +2,14 @@
     import { GiphyFetch } from '@giphy/js-fetch-api'
     import { Grid } from '../../lib/index.js'
     const gf = new GiphyFetch('sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh')
-    const req = gf.trending()
+    const limit = 10
+    const req = gf.trending({ offset: 0, limit })
 </script>
 
 {#await req}
     <p>...waiting</p>
 {:then result}
-    <Grid initialGifs={result.data} width={600} />
+    <Grid initialGifs={result.data} width={600} fetchGifs={(offset) => gf.trending({ offset, limit })} />
 {:catch error}
     <p style="color: red">{error.message}</p>
 {/await}
