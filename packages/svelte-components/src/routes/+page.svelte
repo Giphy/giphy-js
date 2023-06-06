f6f2722fc441b5ca1c0b9b405f69fdef2d730119
giannif <giannif@gmail.com>
2023-06-06 18:32:04 +0200

    svelte use SSR in routes

--- packages/svelte-components/src/routes/+page.svelte
+++ packages/svelte-components/src/routes/+page.svelte
@@ -1,23 +1,17 @@
 <script lang="ts">
-    import { GiphyFetch } from '@giphy/js-fetch-api'
+    import type { GifResult } from '@giphy/js-fetch-api'
     import Loader from '../lib/Loader.svelte'
     import { Gif } from '../lib/index.js'
-    const gf = new GiphyFetch('sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh')
-    const req = gf.gif('ZEU9ryYGZzttn0Cva7')
+    /** @type {import('./$types').PageData} */
+    export let data: GifResult
 </script>
 
-{#await req}
-    <p>...waiting</p>
-{:then result}
-    <Gif
-        gif={result.data}
-        width={300}
-        onGifClick={(gif) => {
-            console.log('clicked', gif.id)
-        }}
-    />
-{:catch error}
-    <p style="color: red">{error.message}</p>
-{/await}
+<Gif
+    gif={data.data}
+    width={300}
+    onGifClick={(gif) => {
+        console.log('clicked', gif.id)
+    }}
+/>
 
 <Loader />
--- packages/svelte-components/src/routes/grid/+page.svelte
+++ packages/svelte-components/src/routes/grid/+page.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
-    import { GiphyFetch } from '@giphy/js-fetch-api'
+    import { GiphyFetch, type GifsResult } from '@giphy/js-fetch-api'
     import { Grid } from '../../lib/index.js'
     const gf = new GiphyFetch('sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh')
     const limit = 10
-    const req = gf.trending({ offset: 0, limit })
+    /** @type {import('./$types').PageData} */
+    export let data: GifsResult
 </script>
 
-{#await req}
-    <p>...waiting</p>
-{:then result}
-    <Grid initialGifs={result.data} width={600} fetchGifs={(offset) => gf.trending({ offset, limit })} />
-{:catch error}
-    <p style="color: red">{error.message}</p>
-{/await}
+<Grid initialGifs={data.data} width={600} fetchGifs={(offset) => gf.trending({ offset, limit })} />
