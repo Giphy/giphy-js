7b05c3934494f051102a0e9cb0d6b8ad6cb37378
giannif <giannif@gmail.com>
2023-06-19 21:59:03 -0700

    esm: use tsup to generate cjs and esm should be more standardized and fix vite

--- packages/react-components/src/index.ts
+++ packages/react-components/src/index.ts
@@ -18,8 +18,5 @@ export * from './components/video/controls/play-pause'
 export * from './components/video/controls/volume'
 export { default as VideoOverlay } from './components/video/video-overlay'
 
-const { version } = require('../package.json')
-
 // send headers with library type and version
 appendGiphySDKRequestHeader(`X-GIPHY-SDK-NAME`, 'ReactSDK')
-appendGiphySDKRequestHeader(`X-GIPHY-SDK-VERSION`, version)
