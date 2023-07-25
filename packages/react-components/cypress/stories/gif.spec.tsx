3be22d55140893ffaca5f32906d54c49df1c81d3
Gianni Ferullo <giannif@gmail.com>
2023-07-24 20:05:15 -0700

    cypress: fix tests

--- packages/react-components/cypress/stories/gif.spec.tsx
+++ packages/react-components/cypress/stories/gif.spec.tsx
@@ -1,42 +1,23 @@
 import { composeStories } from '@storybook/react'
 import * as React from 'react'
-
 import * as stories from '../../stories/gif.stories'
 import {
-    GifTestUtilsContext,
+    checkGifIsVisible,
     checkGifKeyboardEvents,
     checkGifMouseEvents,
     checkGifSeen,
     setupGifTestUtils,
 } from '../utils/gif-test-utils'
 
-const storiesGifIds = {
-    Gif: 'ZEU9ryYGZzttn0Cva7',
-    GifWithVideoOverlay: 'D068R9Ziv1iCjezKzG',
-    GifWithVideoOverlayFillVideo: '3BNRWBatePBETD7Bfg',
-    GifNoBorderRadius: 'ZEU9ryYGZzttn0Cva7',
-    Sticker: 'l1J9FvenuBnI4GTeg',
-    CustomPingbackGif: 'ZEU9ryYGZzttn0Cva7',
-} as const
-
-const testCases = Object.values(composeStories(stories)).map((Story) => [
-    // The ! is necessary in Typescript only, as the property is part of a partial type
-    Story.storyName!,
-    Story,
-])
-
+const testCases = Object.values(composeStories(stories)).map((Story) => [Story.storyName!, Story])
 describe('Gif', () => {
     testCases.forEach(([name = '', Story]) => {
-        let gifTestUtilsCtx: GifTestUtilsContext
-
-        before(() => {
-            gifTestUtilsCtx = setupGifTestUtils(storiesGifIds.Gif)
-        })
-
         it(`Story: ${name}`, () => {
+            // @ts-ignore
+            const gifTestUtilsCtx = setupGifTestUtils(Story.args.id)
             const options = { takeSnapshots: false } // snapshotNamePrefix: `Stories / Gif / ${name}` }
             cy.mount(<Story {...gifTestUtilsCtx.events} />)
-            // checkGifIsVisible(gifTestUtilsCtx)
+            checkGifIsVisible(gifTestUtilsCtx)
             checkGifSeen(gifTestUtilsCtx, options)
             checkGifMouseEvents(gifTestUtilsCtx, options)
             checkGifKeyboardEvents(gifTestUtilsCtx, options)
