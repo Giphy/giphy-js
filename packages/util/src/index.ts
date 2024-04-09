07ef010750cefb6542c411bdde700502f3464082
Nick Santaniello <nicksantan@giphy.com>
2024-04-09 13:03:20 -0700

    Restored construct moat data file

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -1,5 +1,6 @@
 export { default as bestfit, setRenditionScaleUpMaxPixels } from './bestfit'
 export * from './collections'
+export * from './construct-moat-data'
 export { default as getClientRect } from './get-client-rect-from-el'
 export { default as getPingbackId } from './get-pingback-id'
 export {
