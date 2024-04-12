d8be1d62aa5cfde3611b1c9aa823d6106d0b7f4a
Gianni Ferullo <giannif@gmail.com>
2024-04-12 17:36:09 -0400

    deps: issues with dompurify imports

--- packages/util/src/tracking-pixel.ts
+++ packages/util/src/tracking-pixel.ts
@@ -1,10 +1,10 @@
-import { sanitize } from 'dompurify'
+import DOMPurify from 'dompurify'
 
 const injectTrackingPixel = (tags: string[] = []) => {
     tags.forEach((tag) => {
         const el = document.createElement('html')
         tag = tag.replace('%%CACHEBUSTER%%', Date.now().toString())
-        el.innerHTML = sanitize(tag)
+        el.innerHTML = DOMPurify.sanitize(tag)
         const pixel = el.querySelector('img')
         if (pixel) {
             document?.querySelector('head')?.appendChild(pixel)
